6947a2ac5fe4ab6e42ba2c559dfbe9cc4b62f0e2
Melon West <i.melonpub@gmail.com>
2023-09-19 14:32:11 +0600

    fixing mobile sliders

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const slideInterval = 0;// Интервал между сменой слайдо
 
 function nextSlide() {
     slider.style.transitionTimingFunction = 'linear';
-    slider.style.left = -100 + '%'
+    slider.style.left = -200 + '%'
 }
 
 
@@ -22,13 +22,13 @@ counter = 0;
 const slider2 = document.querySelector('.contentLine2')
 document.querySelector('#next2').addEventListener('click', function(){
     counter++
-    if (counter>1){counter=0}
+    if (counter>3){counter=0}
     slider2.style.transition = 'all ease 1s'
     slider2.style.left = -400*counter + 'px'
 });
 document.querySelector('#prev2').addEventListener('click', function(){
     counter--
-    if (counter<0){counter=1}
+    if (counter<0){counter=3}
     slider2.style.left = -400*counter + 'px'
 });
 
@@ -37,12 +37,12 @@ counter6 = 0;
 const slider6 = document.querySelector('.sliderLine6')
 document.querySelector('#next6').addEventListener('click', function(){
     counter6++
-    if (counter6>2){counter6=0}
+    if (counter6>3){counter6=0}
     slider6.style.left = -485*counter6 + 'px'
 });
 document.querySelector('#prev6').addEventListener('click', function(){
     counter6--
-    if (counter6<0){counter6=2}
+    if (counter6<0){counter6=3}
     slider6.style.left = -485*counter6 + 'px'
 });
 
@@ -50,12 +50,12 @@ counter7 = 0;
 const slider7 = document.querySelector('.sliderLine7')
 document.querySelector('#next7').addEventListener('click', function(){
     counter7++
-    if (counter7>2){counter7=0}
+    if (counter7>3){counter7=0}
     slider7.style.left = -380*counter7 + 'px'
 });
 document.querySelector('#prev7').addEventListener('click', function(){
     counter7--
-    if (counter7<0){counter7=2}
+    if (counter7<0){counter7=3}
     slider7.style.left = -380*counter7 + 'px'
 });
 
@@ -82,4 +82,33 @@ document.querySelector('.dialog').addEventListener('click', function(){
 });
 document.querySelector('.modal').addEventListener('click', function(){
     dialog.style.display = 'none';
+});
+
+
+
+counterM = 0;
+const slider2M = document.querySelector('.contentLine2M')
+document.querySelector('#next2M').addEventListener('click', function(){
+    counterM++
+    if (counterM>3){counterM=0}
+    slider2M.style.transition = 'all ease 1s'
+    slider2M.style.left = -400*counterM + 'px'
+});
+document.querySelector('#prev2M').addEventListener('click', function(){
+    counterM--
+    if (counterM<0){counterM=3}
+    slider2M.style.left = -400*counterM + 'px'
+});
+
+counter6M = 0;
+const slider6M = document.querySelector('.sliderLine6M')
+document.querySelector('#next6M').addEventListener('click', function(){
+    counter6M++
+    if (counter6M>3){counter6M=0}
+    slider6M.style.left = -372*counter6M + 'px'
+});
+document.querySelector('#prev6M').addEventListener('click', function(){
+    counter6M--
+    if (counter6M<0){counter6M=3}
+    slider6M.style.left = -372*counter6M + 'px'
 });
